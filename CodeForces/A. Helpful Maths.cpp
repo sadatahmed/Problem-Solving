8d5c3107b82bbfd4c1d245c5c147251b5f5b35c3
sadatahmed <sadatahmed52@gmail.com>
2021-10-06 02:14:15 +0600

    Day-12

---
+++ CodeForces/A. Helpful Maths.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string str;
+    cin>>str;
+    for(int i=0; i<str.length(); i++){
+        if(i%2!=0){
+
+        }
+    }
+
+    return 0;
+}
