da996b28ae03de9402420ead586d6c9b35d02c16
sadatahmed <sadatahmed52@gmail.com>
2021-10-08 20:38:42 +0600

    Day-14

---
+++ CodeForces/A. Calculating Function.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    long long int n, total=0;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<n/2;
+    }else{
+        cout<<(-1)*(n+1)/2;
+    }
+
+    return 0;
+}
