c9c45e951bd4027512d282bf7bbb4c5a6f330327
sadatahmed <sadatahmed52@gmail.com>
2021-10-09 16:25:06 +0600

    Day-14

---
+++ CodeForces/A. Consecutive Sum Riddle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    long long int ara;
+    for(int i=0; i<n; i++){
+        cin>>ara;
+
+        cout<<-(ara-1)<<" "<<ara<<endl;
+    }
+
+
+
+    return 0;
+}
