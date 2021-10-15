805b5e0e2002b43333610c175e09724308548595
sadatahmed <sadatahmed52@gmail.com>
2021-10-16 03:05:34 +0600

    Day-20

--- CodeForces/B. Make it Divisible by 25.cpp
+++ CodeForces/B. Make it Divisible by 25.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Not completed the problem...!
-void dividesBy(int num){
-    int ara[100];
-    int count=0, int i=0;
-    if(num%25==0) cout<<count<<endl;
-    else{
-        while(num<=0){
-            ara[i]=num%10;
-            num=num/10;
-            i++;
+int cont=0;
+
+void dividesBy(string num){
+    int result=0;
+    for(int i=0; i<num.size(); i++){
+        for(int j=i+1; j<num.size(); j++){
+            if(((num[i]-'0')*10+(num[j]-'0'))%25==0){
+                result=num.size()-i-2;
+            }
         }
     }
-
+    cout<<result<<endl;
 }
 
 int main(){
     int n;
+    string number;
     cin>>n;
-    int number;
 
     for(int i=0; i<n; i++){
         cin>>number;
